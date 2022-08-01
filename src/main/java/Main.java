007b0e6aa6a8bdac2f6781bbaab8d1a94befb5bb
Sevka <sevkacode@gmail.com>
2022-08-01 03:47:33 +0300

    first commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -25,43 +25,41 @@ class Student {
 
     @Override
     public String toString() {
-        return "Student: \t" +
+        return "\n" + "Student: \t" +
                 name +
                 "\t| age:\t" + age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Student)) return false;
+        Student student = (Student) o;
+        return age == student.age && Objects.equals(name, student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
     }
 }
 
 public class Main {
     public static void main(String[] args) {
 
-//        List<Student> students = new ArrayList<>();
-//
-//        Student st1 = new Student("Admin", 1977);
-//        students.add(st1);
-//        students.add(new Student("Sevka", 1996));
-//        students.add(new Student("Albert", 2003));
-//        students.add(new Student("Vovan", -200));
-//        students.add(new Student("Emot", 2007));
-//        students.add(new Student(null, 0));
-//        students.add(new Student("Митя", 2022));
-//        students.add(new Student("AaAaz", 101001101));
-//        students.add(new Student(null, 1488));
-//        students.add(new Student(null, 131));
-//
-//        boolean a = students.add(new Student("Test", 0));
-//        System.out.println(a);
+        List<Student> students = new ArrayList<>();
 
-//        ObjectOutputStream oups = new ObjectOutputStream();
-//
-//        students.stream()
-//                .filter(a -> a.getAge() > 1950)
-//                .filter(a -> a.getAge() < 2022)
-//                .forEach(System.out::println);
-
-        List<Integer> numb = new ArrayList<>(Arrays.asList(1,2,3,4,5,6));
-        ListIterator<Integer> iter = numb.listIterator();
-        iter.add(7);
-        System.out.println(numb);
+        Student st1 = new Student("Admin", 1977);
+        students.add(st1);
+        students.add(new Student("Sevka", 1996));
+        students.add(new Student("Albert", 2003));
+        students.add(new Student("Vovan", -200));
+        students.add(new Student("Emot", 2007));
+        students.add(new Student(null, 0));
+        students.add(new Student("Митя", 2022));
+        students.add(new Student("AaAaz", 101001101));
+        students.add(new Student(null, 1488));
+        students.add(new Student(null, 131));
     }
 
 
